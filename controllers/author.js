464bc351b163b9e244ebed2d5512b82d84cb66ed
Sam Mussell <sam@streetsaheadllc.com>
2011-09-21 22:36:52 -0600

    added oauth2 for github
    It doesn't do anything yet, but the basics are there.

--- controllers/author.js
+++ controllers/author.js
@@ -1,5 +1,8 @@
 var lazybum = require('lazybum'),
-	Controller = lazybum.get('Controller');
+	Controller = lazybum.get('Controller'),
+	
+	https = require('https'),
+	qs = require('querystring');
 	
 var log = lazybum.getLogger(module);
 
@@ -20,6 +23,20 @@ author.prototype.index_post = function(urlParts, query, postData) {
 	
 };
 
+var GH_CLIENT_ID = '562559d16db0fdbaf0f9';
+var GH_SECRET = 'fd0b9d3f3b9a631c24b2727b3a2351b62aba78fa';
+var GH_BASE = 'github.com';
+var GH_AUTHORIZE = '/login/oauth/authorize';
+var GH_ACCESS_TOKEN = '/login/oauth/access_token';
+
+author.prototype.oauth_get = function(urlParts, query) {
+	var request = 'https://' + GH_BASE + GH_AUTHORIZE + '?' +
+	 			'client_id=' + GH_CLIENT_ID + '&' +
+				'redirect_url=' + 'http://localhost:8888/author/oauth_callback';
+				
+	this.redirect(request);
+}
+
 author.prototype.edit_get = function(urlParts, query){
 	var data = {
 		innerTemplate: 'author/edit_form',
@@ -28,6 +45,33 @@ author.prototype.edit_get = function(urlParts, query){
   this.writeResponse(data, 'index')
 }
 
+
+author.prototype.oauth_callback_get = function(urlParts, query) {
+ 	var response = qs.parse(query);
+
+	var request =  'client_id=' + GH_CLIENT_ID + '&' +
+				'client_secret=' + GH_SECRET + '&' + 
+				'code=' + response.code;
+	var req = https.request({
+		host: GH_BASE,
+		path: GH_ACCESS_TOKEN,
+		method: 'POST'
+	},function(res) {
+		res.on('data', function(data) {
+			var accessToken = qs.parse(data.toString('utf8')).access_token;
+			https.get({host: GH_BASE, path: '/user.json?access_token=' + accessToken},
+						function(res) {
+							res.on('data', function(data) { console.log(data.toString('utf8')) });
+						});
+		})
+	});
+	
+	req.write(request);
+	req.end();
+	
+	
+	//this.writeResponse({author:[]}, 'author/list');
+}
 
 author.prototype.edit_post = function(urlParts, query, postData){
 	var that = this;

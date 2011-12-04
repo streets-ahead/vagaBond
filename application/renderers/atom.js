fdcb5d6406330c051c89fbfa649ce4271318e94b
Sam Mussell <sam@streetsaheadllc.com>
2011-12-04 15:50:18 -0700

    added atom feed, hopefully

---
+++ application/renderers/atom.js
@@ -0,0 +1,34 @@
+var Renderer = require('Renderer'),
+	FeedBum = require('../FeedBum/FeedBum'),
+	lbConfig = require('lbConfig');
+
+var atom = Renderer.extend(function() {
+	atom.super_.apply(this, arguments);
+});
+
+module.exports = atom;
+
+atom.prototype.render = function(obj) {
+	feed = new FeedBum(FeedBum.ATOM);
+	feed.setTitle("Streets Ahead");
+	feed.setLink(lbConfig.getConfig().baseURL);
+	feed.setChannelElement('author', {'name' : 'Streets Ahead LLC.', 'email':'team@streetsaheadllc.com'});
+	//feed.setDate('updated', obj.posts[0].date);
+
+	for(var i = 0; i < obj.posts.length; i++ ) {
+		newItem = feed.createNewItem();
+		//Add elements to the feed item
+		//Use wrapper functions to add common feed elements
+		newItem.setTitle(obj.posts[i].title);
+		newItem.setLink(lbConfig.getConfig().baseURL + 'article/' + obj.posts[i].page_url);
+		newItem.setDate(obj.posts[i].date);
+		//Internally changed to "summary" tag for ATOM feed
+		newItem.setDescription(obj.posts[i].body);
+		//Now add the feed item	
+		feed.addItem(newItem);
+	}
+
+	this.endResponse(feed.generateFeed());
+}; 
+
+atom.CONTENT_TYPE = "application/atom+xml";
